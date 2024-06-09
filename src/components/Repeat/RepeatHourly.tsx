21a25f9be5fee9ea0bc358263edc4a4c4eeaad18
Dario Cantatore <dario.cantatore@gmail.com>
2024-06-09 10:46:23 -0400

    Update hourly view

--- src/components/Repeat/RepeatHourly.tsx
+++ src/components/Repeat/RepeatHourly.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 
 import TextField from "@mui/material/TextField";
+import { Frequency } from "rrule";
+import { Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import { HourlyRepeatDetails } from "./Repeat.types";
 
 interface RepeatHourlyProps {
-  defaultValue?: number;
+  defaultValue?: HourlyRepeatDetails;
   onChange: (value: number) => void;
 }
 
 const RepeatHourly = (
-  { defaultValue = 1, onChange }: RepeatHourlyProps,
+  { defaultValue = { interval: 1, frequency: Frequency.HOURLY }, onChange }: RepeatHourlyProps,
 ) => (
-  <TextField
-    id="outlined-basic"
-    label="Repeat every"
-    variant="outlined"
-    type="number"
-    defaultValue={defaultValue}
-    onChange={(e) => onChange(parseInt(e.target.value, 10))}
-  />
+  <Stack direction="row" spacing={2} alignItems="center">
+    <Typography>Every</Typography>
+    <TextField
+      id="outlined-basic"
+      label=""
+      variant="outlined"
+      type="number"
+      defaultValue={defaultValue?.interval}
+      onChange={(e) => console.log(e.target.value)}
+    />
+    <Typography>hours(s)</Typography>
+  </Stack>
+
 );
 
 export default RepeatHourly;
