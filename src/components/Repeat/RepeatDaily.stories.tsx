5b897abf4fd702597681a741cfa4b833afea5f68
Dario Cantatore <dario.cantatore@gmail.com>
2024-06-15 12:05:42 -0400

    Make story use store

--- src/components/Repeat/RepeatDaily.stories.tsx
+++ src/components/Repeat/RepeatDaily.stories.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import RepeatDaily from "./RepeatDaily";
+import useBuilderStore from "../../store/builderStore";
 
 export default {
   title: "Repeat/RepeatDaily",
   component: RepeatDaily,
 } as Meta<typeof RepeatDaily>;
 
-const Template: StoryFn<typeof RepeatDaily> = (args) => <RepeatDaily {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  value: { interval: 1 },
-  onChange: (value) => console.log(value),
+const Template: StoryFn<typeof RepeatDaily> = () => {
+  const builderStore = useBuilderStore();
+  return (
+    <RepeatDaily value={builderStore.repeatDetails} onChange={builderStore.setRepeatDetails} />
+  );
 };
+
+export const Primary = Template;
