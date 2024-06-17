1ad476b4a0c0e45fcabe581793091f4124ef8c74
Dario Cantatore <dario.cantatore@gmail.com>
2024-06-17 08:17:34 -0400

    Add ability to init with string and have onchange passed in

--- src/store/builderStore.tsx
+++ src/store/builderStore.tsx
@@ -1,16 +1,16 @@
 import { create } from "zustand";
 import * as Yup from "yup";
-import { Frequency } from "rrule";
+import { Frequency, RRule } from "rrule";
 import { DateTime } from "luxon";
 import { RepeatDetails } from "../components/Repeat/Repeat.types";
 import getValidationSchema from "../validation/validationSchema";
 import { EndDetails, EndType } from "../components/End/End.types";
-import { buildRRuleString } from "../utils/buildRRule";
+import { buildRRuleString } from "../utils/buildRRuleString";
 
 interface BuilderState {
   repeatDetails: RepeatDetails;
   frequency: Frequency;
-  startDate: DateTime;
+  startDate: DateTime | null;
   validationErrors: Record<string, string>;
   endDetails: EndDetails;
   RRuleString?: string;
@@ -22,8 +22,11 @@ interface BuilderActions {
   setRepeatDetails: (details: RepeatDetails) => void;
   validateForm: () => Promise<boolean>;
   setEndDetails: (details: EndDetails) => void;
-  setStartDate: (startDate: DateTime) => void;
+  setStartDate: (startDate: DateTime | null) => void;
   buildRRuleString: () => void;
+  onChange?: (rruleString: string) => void;
+  setOnChange: (onChange: (rruleString: string) => void) => void;
+  setStoreFromRRuleString: (rruleString: string) => void;
 }
 
 const initialState: BuilderState = {
@@ -38,20 +41,40 @@ const useBuilderStore = create<BuilderState & BuilderActions>((set, get) => ({
   ...initialState,
   validationErrors: {},
   setFrequency: (frequency) => {
+    // clear repeat details when changing frequency
     set({ frequency });
     set({ repeatDetails: { } as RepeatDetails });
+    // clear validation errors
     set({ validationErrors: {} });
+
+    // rebuild the rrule string
+    get().buildRRuleString();
   },
   setStartDate: (startDate) => {
     const { endDate } = get().endDetails;
     // don't allow end date to be before start date, add one 1 day
-    if (endDate && startDate > endDate) {
+    if (endDate && startDate && startDate > endDate) {
       set({ endDetails: { ...get().endDetails, endDate: startDate.plus({ days: 1 }) } });
     }
+
+    // set the value
     set({ startDate });
+    // clear validation errors
+    set({ validationErrors: {} });
+    // rebuild the rrule string
+    get().buildRRuleString();
   },
-  setEndDetails: (details) => set({ endDetails: details }),
-  setRepeatDetails: (details) => set({ repeatDetails: details }),
+  setEndDetails: (details) => {
+    set({ endDetails: details });
+
+    // rebuild the rrule string
+    get().buildRRuleString();
+  },
+  setRepeatDetails: (details) => {
+    set({ repeatDetails: details });
+    // rebuild the rrule string
+    get().buildRRuleString();
+  },
   validateForm: async () => {
     const { repeatDetails, frequency } = get();
     const validationSchema = getValidationSchema(frequency);
@@ -84,6 +107,45 @@ const useBuilderStore = create<BuilderState & BuilderActions>((set, get) => ({
     });
 
     set({ RRuleString: output });
+
+    // if there is an onChange function, call it with the output
+    const { onChange } = get();
+    if (onChange) onChange(output);
+  },
+  setOnChange: (onChange) => set({ onChange }),
+  setStoreFromRRuleString: (rruleString) => {
+    const parsedObj = RRule.parseString(rruleString);
+    const {
+      setFrequency, setStartDate, setEndDetails, setRepeatDetails,
+    } = get();
+
+    // set the frequency
+    if (parsedObj.freq) {
+      setFrequency(parsedObj.freq);
+    }
+    // set the start date
+    if (parsedObj.dtstart) {
+      setStartDate(DateTime.fromJSDate(parsedObj.dtstart));
+    }
+
+    // set the end date
+    if (parsedObj.until) {
+      setEndDetails({ endingType: EndType.ON, endDate: DateTime.fromJSDate(parsedObj.until) });
+    } else if (parsedObj.count) {
+      setEndDetails({ endingType: EndType.AFTER, occurrences: parsedObj.count });
+    }
+
+    // set the repeat details
+    const repeatDetails = {
+      interval: parsedObj.interval,
+      byDay: parsedObj.byweekday,
+      byMonthDay: parsedObj.bymonthday,
+      byMonth: parsedObj.bymonth,
+      bySetPos: parsedObj.bysetpos,
+    };
+
+    // TODO finish fixing the types
+    setRepeatDetails(repeatDetails);
   },
 }));
 
