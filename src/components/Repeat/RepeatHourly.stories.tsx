5ca3b1314fefff1a7d326e5f73e1b1ac3e132c57
Dario Cantatore <dario.cantatore@gmail.com>
2024-06-23 12:05:08 -0400

    Fix story

--- src/components/Repeat/RepeatHourly.stories.tsx
+++ src/components/Repeat/RepeatHourly.stories.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import { Frequency } from "rrule";
 import RepeatHourly from "./RepeatHourly";
+import useBuilderStore from "../../store/builderStore";
 
 export default {
   title: "Repeat/RepeatHourly",
   component: RepeatHourly,
 } as Meta<typeof RepeatHourly>;
 
-const Template: StoryFn<typeof RepeatHourly> = (args) => <RepeatHourly {...args} />;
-
-export const Primary = Template.bind({});
-Primary.args = {
-  value: { interval: 1 },
-  onChange: (value) => console.log(value),
+const Template: StoryFn<typeof RepeatHourly> = () => {
+  const builderStore = useBuilderStore();
+  return (
+    <RepeatHourly value={builderStore.repeatDetails} onChange={builderStore.setRepeatDetails} />
+  );
 };
+
+export const Primary = Template;
