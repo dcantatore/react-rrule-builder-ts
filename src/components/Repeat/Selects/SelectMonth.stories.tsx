d0b89bbadfbfa357eec9f43aa06cd9f379bde950
Dario Cantatore <dario.cantatore@gmail.com>
2024-06-23 11:44:23 -0400

    Create SelectMonth.stories.tsx

---
+++ src/components/Repeat/Selects/SelectMonth.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Meta, StoryFn } from "@storybook/react";
+import SelectMonth from "./SelectMonth";
+import useBuilderStore from "../../../store/builderStore";
+
+export default {
+  title: "Selects/SelectMonth",
+  component: SelectMonth,
+} as Meta<typeof SelectMonth>;
+
+const Template: StoryFn<typeof SelectMonth> = (args) => {
+  const builderStore = useBuilderStore();
+  const { disabled } = args;
+  return (
+    <SelectMonth value={builderStore.repeatDetails} onChange={builderStore.setRepeatDetails} disabled={disabled} />
+  );
+};
+
+export const Primary = Template;
+Primary.args = {
+  disabled: false,
+};
