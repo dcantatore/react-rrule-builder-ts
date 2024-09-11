5ff2adc2f3f528e79de32726cd78f024014a06b0
Adrian Kaiser <kaisera.waterly@gmail.com>
2024-09-11 00:31:14 -0500

    test commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as RRuleBuilder } from "./components/RRuleBuilder/RRuleBuilder";
 //
 export { default as useBuilderStore } from "./store/builderStore";
+
+// I AM THE MASTER COMMANDER!
