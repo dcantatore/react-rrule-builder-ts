9798e45449b8dada4641c8e031b7b6748fd2a56d
Dario Cantatore <dario.cantatore@gmail.com>
2026-02-27 12:14:07 -0500

    ix package.json entry points and add build script

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
 export { default as RRuleBuilder } from "./src/components/RRuleBuilder/RRuleBuilder";
 export { default as useBuilderStore } from "./src/store/builderStore";
 export { BuilderStoreProvider } from "./src/store/builderStore";
+
+// Types
+export {
+  Weekday,
+  Months,
+  MonthBy,
+  YearlyBy,
+  WeekdayExtras,
+  OnThe,
+  AllWeekDayOptions,
+} from "./src/components/Repeat/Repeat.types";
+export type { AllRepeatDetails } from "./src/components/Repeat/Repeat.types";
+export { EndType } from "./src/components/End/End.types";
+export type { EndDetails } from "./src/components/End/End.types";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 export { default as RRuleBuilder } from "./components/RRuleBuilder/RRuleBuilder";
 export { default as useBuilderStore } from "./store/builderStore";
 export { BuilderStoreProvider } from "./store/builderStore";
+
+// Types
+export {
+  Weekday,
+  Months,
+  MonthBy,
+  YearlyBy,
+  WeekdayExtras,
+  OnThe,
+  AllWeekDayOptions,
+} from "./components/Repeat/Repeat.types";
+export type { AllRepeatDetails } from "./components/Repeat/Repeat.types";
+export { EndType } from "./components/End/End.types";
+export type { EndDetails } from "./components/End/End.types";
